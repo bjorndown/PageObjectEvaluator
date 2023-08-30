583abac79a54d39091e6c7ad4d098d426d964dc0
bjorndown <bjorndown@smoler.ch>
2023-08-30 16:41:22 +0200

    Fix verifier result: do not use internal class to load icon.

--- src/main/java/com/intellij/plugins/pageObjectEvaluator/PageObjectRunConfigType.java
+++ src/main/java/com/intellij/plugins/pageObjectEvaluator/PageObjectRunConfigType.java
@@ -2,18 +2,13 @@ package com.intellij.plugins.pageObjectEvaluator;
 
 import com.intellij.execution.configurations.ConfigurationTypeBase;
 import com.intellij.execution.configurations.ConfigurationTypeUtil;
-import com.intellij.openapi.util.IconLoader;
-import com.intellij.ui.IconManager;
 import org.jetbrains.annotations.NotNull;
-
-import javax.swing.*;
 
 public class PageObjectRunConfigType extends ConfigurationTypeBase {
     static final String ID = "PageObjectEvaluator";
-    static final Icon icon = IconLoader.createLazy(() -> IconManager.getInstance().getIcon("META-INF/pluginIcon.svg", PageObjectRunConfigType.class));
 
     protected PageObjectRunConfigType() {
-        super(ID, "PageObjectEvaluator", "Evaluates a PageObject without running a full selenium test test", icon);
+        super(ID, "PageObjectEvaluator", "Evaluates a PageObject without running a full selenium test test", PoeIcons.PLUGIN_ICON);
         addFactory(new RunConfigurationFactory(this));
     }
 
---
+++ src/main/java/com/intellij/plugins/pageObjectEvaluator/PoeIcons.java
@@ -0,0 +1,12 @@
+package com.intellij.plugins.pageObjectEvaluator;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public final class PoeIcons {
+    private PoeIcons() {
+    }
+
+    public static final Icon PLUGIN_ICON = IconLoader.getIcon("META-INF/pluginIcon.svg", PoeIcons.class);
+}
